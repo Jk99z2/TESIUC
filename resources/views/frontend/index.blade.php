71fbc5ca413f6bc1a5a6130867fcc79793a7e540
Jk99z2 <eochoa11@ucol.mx>
2023-11-27 21:43:25 -0600

    Update the index page, adding more local resources and editing information of the app in settings

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,67 +1,116 @@
 @extends('frontend.layouts.app')
 
-@section('title') {{app_name()}} @endsection
+@section('title')
+    {{ app_name() }}
+@endsection
 
 @section('content')
-
-<section class="bg-gray-100 mb-20">
-    <div class="container mx-auto flex px-1 sm:px-20 py-20 md:flex-row flex-col items-center">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-            <img class="object-cover object-center rounded" alt="hero" src="{{ asset('img/tesiuc-square.png') }}">
-        </div>
-        <div class="lg:flex-grow md:w-1/2 px-4 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 class="title-font sm:text-8xl text-5xl mb-4 font-medium text-gray-800">
-                {{ app_name() }}
-            </h1>
-            <p class="mb-8 sm:text-4xl text-3xl">
-                {!! setting('meta_description') !!}
-            </p>
-            <div class="flex justify-center">
-                <a href="https://github.com/nasirkhan" class="inline-flex text-white bg-gray-700 border-0 py-2 px-6 focus:outline-none hover:bg-gray-800 rounded text-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="w-8 h-8" viewBox="0 0 18 18">
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                    </svg>
-                    Github
-                </a>
-                <a href="https://nasirkhn.com" class="ml-4 outline outline-1 outline-gray-800 inline-flex text-gray-700 hover:text-gray-100 bg-gray-200 hover:bg-gray-700 py-2 px-6 focus:outline-none rounded text-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="w-8 h-8" viewBox="0 0 18 18">
-                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855-.143.268-.276.56-.395.872.705.157 1.472.257 2.282.287V1.077zM4.249 3.539c.142-.384.304-.744.481-1.078a6.7 6.7 0 0 1 .597-.933A7.01 7.01 0 0 0 3.051 3.05c.362.184.763.349 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9.124 9.124 0 0 1-1.565-.667A6.964 6.964 0 0 0 1.018 7.5h2.49zm1.4-2.741a12.344 12.344 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332zM8.5 5.09V7.5h2.99a12.342 12.342 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.612 13.612 0 0 1 7.5 10.91V8.5H4.51zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741H8.5zm-3.282 3.696c.12.312.252.604.395.872.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a6.696 6.696 0 0 1-.598-.933 8.853 8.853 0 0 1-.481-1.079 8.38 8.38 0 0 0-1.198.49 7.01 7.01 0 0 0 2.276 1.522zm-1.383-2.964A13.36 13.36 0 0 1 3.508 8.5h-2.49a6.963 6.963 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667zm6.728 2.964a7.009 7.009 0 0 0 2.275-1.521 8.376 8.376 0 0 0-1.197-.49 8.853 8.853 0 0 1-.481 1.078 6.688 6.688 0 0 1-.597.933zM8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855.143-.268.276-.56.395-.872A12.63 12.63 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.963 6.963 0 0 0 14.982 8.5h-2.49a13.36 13.36 0 0 1-.437 3.008zM14.982 7.5a6.963 6.963 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008h2.49zM11.27 2.461c.177.334.339.694.482 1.078a8.368 8.368 0 0 0 1.196-.49 7.01 7.01 0 0 0-2.275-1.52c.218.283.418.597.597.932zm-.488 1.343a7.765 7.765 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z" />
-                    </svg>
-                    Website
-                </a>
+    <section class="bg-gray-100 mb-20">
+        <div class="container mx-auto flex px-1 sm:px-20 py-20 md:flex-row flex-col items-center">
+            <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <img class="object-cover object-center rounded" alt="hero" src="{{ asset('img/tesiuc-square.png') }}">
             </div>
+            <div
+                class="lg:flex-grow md:w-1/2 px-4 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 class="title-font sm:text-8xl text-5xl mb-4 font-medium text-gray-800">
+                    {{ app_name() }}
+                </h1>
+                <p class="mb-8 sm:text-4xl text-3xl">
+                    {!! setting('meta_description') !!}
+                </p>
+                <div class="flex justify-center">
+                    <a href="https://github.com/Jk99z2/TESIUC"
+                        class="inline-flex text-white bg-gray-700 border-0 py-2 px-8 focus:outline-none hover:bg-gray-800 rounded text-lg">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                            class="w-8 h-8" viewBox="0 0 18 18">
+                            <path
+                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.40.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.20-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.20-.36-1.02.08-2.12 0 0 .67-.21 2.20.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.20-.82 2.20-.82.44 1.10.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.20 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                        </svg>
+                        Github
+                    </a>
+                </div>
 
-            @include('frontend.includes.messages')
+                @include('frontend.includes.messages')
 
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section class="mb-20">
-    <div class="container mx-auto flex px-5 py-10 items-center justify-center flex-col">
-        <div class="text-center lg:w-2/3 w-full">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-800">
-                Screenshots of the project
-            </h1>
-            <p class="mb-8 leading-relaxed">
-                In the following section we listed a number of screenshots of different parts of the project, Laravel Starter.
-            </p>
+    <section class="mb-20">
+        <div class="container mx-auto flex flex-col md:flex-row items-center">
+            <div class="flex flex-col w-full justify-center items-start md:text-left md:mr-10">
+                <h1 class="my-4 text-5xl font-bold leading-tight mb-8">
+                    ¿Qué es TESIUC?
+                </h1>
+                <p class="leading-normal text-2xl mb-8 ml-3 text-justify">
+                    TESIUC es la plataforma de control y seguimiento de tesis en línea de la Universidad de Colima. Esta
+                    plataforma integra herramientas para la gestión de tesis, la gestión de presentaciones, la revisión e
+                    interacción entre autores, así como el seguimiento y evaluación de los estudiantes.
+                </p>
+            </div>
+            <div class="w-full md:w-1/3 py-6 text-center rounded">
+                <img class="w-full md:w-4/5 z-50 rounded-lg" src="{{ asset('img/student.jpg') }}">
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<section class="mb-20">
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 p-5">
-        <div class="shadow-lg p-3 sm:p-10 rounded-lg">
-            <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/b9ca9cd8-fa7c-43f0-b54f-47e7c4966d9c" alt="Page preview">
+    <section class="mb-20">
+        <!-- Four columns container with some information about the application -->
+        <div class="container mx-auto px-4">
+            <div class="flex flex-wrap">
+                <!-- Card 1 -->
+                <div class="w-full md:w-4/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                    <div class="px-6 py-6 rounded-lg shadow-lg text-center border-0 bg-white mb-6">
+                        <div
+                            class="w-20 h-20 p-3 shadow-lg rounded-full bg-gray-800 mx-auto mb-4 flex items-center justify-center">
+                            <i class="fas fa-graduation-cap text-xl text-white"></i>
+                        </div>
+                        <h6 class="text-xl font-semibold">Gestiona tu tesis</h6>
+                        <p class="mt-2 text-gray-600">
+                            TESIUC te permite gestionar tu tesis de manera sencilla y eficiente.
+                        </p>
+                    </div>
+                </div>
+                <!-- Card 2 -->
+                <div class="w-full md:w-4/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                    <div class="px-6 py-6 rounded-lg shadow-lg text-center border-0 bg-white mb-6">
+                        <div
+                            class="w-20 h-20 p-3 shadow-lg rounded-full bg-gray-800 mx-auto mb-4 flex items-center justify-center">
+                            <i class="fas fa-calendar-alt text-xl text-white"></i>
+                        </div>
+                        <h6 class="text-xl font-semibold">Programa tus presentaciones</h6>
+                        <p class="mt-2 text-gray-600">
+                            TESIUC te permite programar tus presentaciones de tesis de manera organizada.
+                        </p>
+                    </div>
+                </div>
+                <!-- Card 3 -->
+                <div class="w-full md:w-4/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                    <div class="px-6 py-6 rounded-lg shadow-lg text-center border-0 bg-white mb-6">
+                        <div
+                            class="w-20 h-20 p-3 shadow-lg rounded-full bg-gray-800 mx-auto mb-4 flex items-center justify-center">
+                            <i class="fas fa-comments text-xl text-white"></i>
+                        </div>
+                        <h6 class="text-xl font-semibold">Interactúa con otros autores</h6>
+                        <p class="mt-2 text-gray-600">
+                            TESIUC te permite interactuar y colaborar con otros autores de tesis.
+                        </p>
+                    </div>
+                </div>
+                <!-- Card 4 -->
+                <div class="w-full md:w-4/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                    <div class="px-6 py-6 rounded-lg shadow-lg text-center border-0 bg-white mb-6">
+                        <div
+                            class="w-20 h-20 p-3 shadow-lg rounded-full bg-gray-800 mx-auto mb-4 flex items-center justify-center">
+                            <i class="fas fa-chart-bar text-xl text-white"></i>
+                        </div>
+                        <h6 class="text-xl font-semibold">Realiza seguimiento y evaluación</h6>
+                        <p class="mt-2 text-gray-600">
+                            TESIUC te permite realizar el seguimiento y evaluación de los estudiantes.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="shadow-lg p-3 sm:p-10 rounded-lg row-span-2">
-            <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/b067e211-1208-49a6-859b-7a6810e3f3bb" alt="Page preview">
-        </div>
-        <div class="shadow-lg p-3 sm:p-10 rounded-lg">
-            <img src="https://github.com/nasirkhan/laravel-starter/assets/396987/413b3c75-4a1f-47e3-8885-bc6bd475213c" alt="Page preview">
-        </div>
-    </div>
-</section>
-
+    </section>
 @endsection
